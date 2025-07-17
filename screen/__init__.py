b03ddd88321e1f5d19dffcd476595e367c5b4c9b
Ghost <rynjs0808@gmail.com>
2025-07-17 21:00:53 +0400

    Add files via upload

---
+++ screen/__init__.py
@@ -0,0 +1,63 @@
+def scan() :
+    try :
+        import pygame
+        print("screen requirment satisfied")
+    except :
+        import platform
+
+        system = platform.system()
+        if system == "Windows" :
+            try :
+                print("Installing Pygame...\n")
+                pygame_install = "pip install pygame"
+                import subprocess
+
+                subprocess.run(pygame_install, shell=True)
+                print("Pygame installed")
+            except :
+                pygame_install = "pip install pygame"
+                print("Unidentified error")
+                print(f"Please install pygame manually in cmd with \"{pygame_install}\"")
+
+    print("\n\nscreen V1.0 -- GitHub repository\n")
+    print("Hello!\n")
+
+scan()
+
+import pygame
+
+class Screen :
+        def __init__(self, width, height,caption) :
+
+            self.width = width
+            self.height = height
+            self.caption = caption
+
+            self.display = pygame.display.set_mode((self.width, self.height))
+
+        def GetScreen(self) :
+            return self.display
+        def info(self) :
+            print("\n\nINFO\n\n")
+            print(f"Width : {self.width}")
+            print(f"Height : {self.height}")
+            print(f"Caption : {self.caption}")
+        def ExitCondition(self) :
+            for event in pygame.event.get() :
+                if event.type == pygame.QUIT :
+                    from sys import exit
+
+                    exit()
+        def GetEvent(self) :
+            self.event = pygame.event.get()
+            return self.event
+        
+
+def Defo(caption) :
+    print("Setting window with default atribute")
+
+    width, height = 700, 600
+    display = Screen(width, height, caption)
+
+    return display
+                
